aea5c96b7f6dfb9122664d3fdd45d63d0130ad04
misigno <efournier92@gmail.com>
2018-11-17 14:14:21 -0500

    disable account chooser in authConfig

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -4,12 +4,7 @@ export const AuthConfig: firebaseui.auth.Config = {
   signInFlow: 'popup',
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    // {
-    //   requireDisplayName: true,
-    //   provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    // },
   ],
-  credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM,
+  credentialHelper: firebaseui.auth.CredentialHelper.NONE,
 };
 
