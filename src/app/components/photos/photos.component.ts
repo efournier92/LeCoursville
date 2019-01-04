d95370102acc0403df3e658cb4325048c60d6f55
misigno <efournier92@gmail.com>
2019-01-03 22:12:19 -0500

    integrate lightgallery.js with photos component

--- src/app/components/photos/photos.component.ts
+++ src/app/components/photos/photos.component.ts
@@ -5,7 +5,71 @@ import { PhotosService } from './photos.service'
 import { Photo } from './photo';
 import { AuthService } from '../auth/auth.service';
 import { User } from '../auth/user';
-import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+import lightGallery from '../lightgallery.js';
+import $ from 'jquery';
+
+declare var lightGallery: any;
+
+declare global {
+  interface Window { lgData: any; }
+}
+
+const options = {
+  // mode: 'fade',  // Type of transition between images. Either 'slide' or 'fade'.
+  // useCSS: true,     // Whether to always use jQuery animation for transitions or as a fallback.
+  // cssEasing: 'ease',   // Value for CSS "transition-timing-function".
+  // easing: 'linear', //'for jquery animation',//
+  speed: 600,      // Transition duration (in ms).
+  // addClass: '',       // Add custom class for gallery.
+
+  // preload: 1,    //number of preload slides. will exicute only after the current slide is fully loaded. ex:// you clicked on 4th image and if preload = 1 then 3rd slide and 5th slide will be loaded in the background after the 4th slide is fully loaded.. if preload is 2 then 2nd 3rd 5th 6th slides will be preloaded.. ... ...
+  showAfterLoad: true,  // Show Content once it is fully loaded.
+  selector: '.light-link',  // Custom selector property insted of just child.
+  // index: false, // Allows to set which image/video should load when using dynamicEl.
+
+  // dynamic: false, // Set to true to build a gallery based on the data from "dynamicEl" opt.
+  // dynamicEl: [],    // Array of objects (src, thumb, caption, desc, mobileSrc) for gallery els.
+
+  // thumbnail: true,     // Whether to display a button to show thumbnails.
+  // showThumbByDefault: false,    // Whether to display thumbnails by default..
+  // exThumbImage: false,    // Name of a "data-" attribute containing the paths to thumbnails.
+  // animateThumb: true,     // Enable thumbnail animation.
+  // currentPagerPosition: 'middle', // Position of selected thumbnail.
+  // thumbWidth: 100,      // Width of each thumbnails
+  // thumbMargin: 5,        // Spacing between each thumbnails 
+
+  controls: true,  // Whether to display prev/next buttons.
+  hideControlOnEnd: false, // If true, prev/next button will be hidden on first/last image.
+  loop: true, // Allows to go to the other end of the gallery at first/last img.
+  auto: true, // Enables slideshow mode.
+  pause: 4000,  // Delay (in ms) between transitions in slideshow mode.
+  escKey: true,  // Whether lightGallery should be closed when user presses "Esc".
+  closable: true,  //allows clicks on dimmer to close gallery
+
+  counter: true, // Shows total number of images and index number of current image.
+  // lang: { allPhotos: 'All photos' }, // Text of labels.
+
+  // mobileSrc: false, // If "data-responsive-src" attr. should be used for mobiles.
+  // mobileSrcMaxWidth: 640,   // Max screen resolution for alternative images to be loaded for.
+  // swipeThreshold: 50,    // How far user must swipe for the next/prev image (in px).
+  enableTouch: true,  // Enables touch support
+  enableDrag: true,  // Enables desktop mouse drag support
+
+  // vimeoColor: 'CCCCCC', // Vimeo video player theme color (hex color code).
+  // youtubePlayerParams: false, // See: https://developers.google.com/youtube/player_parameters
+  // videoAutoplay: true,     // Set to false to disable video autoplay option.
+  // videoMaxWidth: '855px',  // Limits video maximal width (in px).
+
+  // Callbacks el = current plugin object
+  onOpen: function (el) { }, // Executes immediately after the gallery is loaded.
+  onSlideBefore: function (el) { }, // Executes immediately before each transition.
+  onSlideAfter: function (el) { }, // Executes immediately after each transition.
+  onSlideNext: function (el) { }, // Executes immediately before each "Next" transition.
+  onSlidePrev: function (el) { }, // Executes immediately before each "Prev" transition.
+  onBeforeClose: function (el) { }, // Executes immediately before the start of the close process.
+  onCloseAfter: function (el) { }, // Executes immediately once lightGallery is closed.
+
+};
 
 @Component({
   selector: 'app-photos',
@@ -29,8 +93,7 @@ export class PhotosComponent implements OnInit {
   years: Number[];
   showSpinner: boolean = true;
   sortType: string = 'random';
-
-
+  photoGallery: Element;
 
   constructor(
     private photosService: PhotosService,
@@ -39,6 +102,7 @@ export class PhotosComponent implements OnInit {
     this.auth.userObservable.subscribe(
       (user: User) => {
         this.user = user;
+        this.updatePhotoGallery();
       }
     )
     this.loadAllPhotos();
@@ -46,7 +110,6 @@ export class PhotosComponent implements OnInit {
 
   ngOnInit(): void {
     this.years = this.photosService.getYears();
-    console.log("tr", PhotoSwipe);
   }
 
   addPhoto(event: any): void {
@@ -62,20 +125,21 @@ export class PhotosComponent implements OnInit {
     }
   }
 
-  loadNumMorePhotos(numToLoad): void {
+  loadMorePhotos(numToLoad): void {
     for (let i = 0; i < numToLoad; i++) {
       this.loadAnotherPhoto();
     }
     setTimeout(() => {
       this.showSpinner = false;
     }, 1000);
+    this.updatePhotoGallery();
   }
 
   sortPhotosRandomly() {
     this.showSpinner = true;
     this.loadedPhotos = [];
     this.loadablePhotos = this.shufflePhotos(this.allPhotos);
-    this.loadNumMorePhotos(3);
+    this.loadMorePhotos(3);
   }
 
   shufflePhotos(photos: Array<Photo>) {
@@ -92,7 +156,7 @@ export class PhotosComponent implements OnInit {
     this.showSpinner = true;
     this.loadablePhotos = this.allPhotos.sort(sortFunction);
     this.loadedPhotos = [];
-    this.loadNumMorePhotos(3);
+    this.loadMorePhotos(3);
   }
 
   sortPhotosByDateAdded(a: Photo, b: Photo) {
@@ -124,7 +188,7 @@ export class PhotosComponent implements OnInit {
       }
     }
     this.loadedPhotos = [];
-    this.loadNumMorePhotos(3);
+    this.loadMorePhotos(3);
   }
 
   clearSearch() {
@@ -132,12 +196,33 @@ export class PhotosComponent implements OnInit {
     this.sortPhotosRandomly();
   }
 
+  destroyPhotoGallery() {
+    let at = this.photoGallery.getAttribute('lg-uid')
+    window.lgData[at].destroy(true);
+  }
+
+  updatePhotoGallery() {
+    if (this.photoGallery) {
+      this.destroyPhotoGallery();
+      this.photoGallery = null;
+    }
+
+    this.photoGallery = document.getElementById('lightgallery');
+    lightGallery(this.photoGallery, options);
+
+    // if (this.photoGallery)
+    //   this.photoGallery.data('lightGallery').destroy(true);
+    // this.photoGallery = $("#lightgallery");
+    // var gallery = $("#lightgallery").lightGallery({})
+
+  }
+
   loadAllPhotos(): void {
     this.photosService.getAllPhotos().valueChanges().subscribe(
       (photos: Array<Photo>) => {
         this.allPhotos = photos;
         this.sortPhotosRandomly();
-        this.loadNumMorePhotos(3);
+        this.loadMorePhotos(3);
       }
     );
   }
